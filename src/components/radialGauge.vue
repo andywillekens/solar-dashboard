25be3dd8a0044334d64b89ecf956a39ba794f26e
Andy Willekens <andywillekens@gmail.com>
2024-05-10 10:34:33 +0200

    Fixed size

--- src/components/radialGauge.vue
+++ src/components/radialGauge.vue
@@ -15,7 +15,7 @@ const gaugeColors =
 
 const chartOptions = {
   chart: {
-    height: 250,
+    height: 350,
     type: "radialBar",
     offsetY: 0,
   },
@@ -61,10 +61,10 @@ const chartOptions = {
 </script>
 <template>
   <apexchart
+    class="w-[500px]"
     ref="realtimeChart"
     type="radialBar"
     :options="chartOptions"
     :series="[scaleData]"
-    size="400"
   />
 </template>
